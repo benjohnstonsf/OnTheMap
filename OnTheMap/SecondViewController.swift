b0969e3fa8de4f159e3e2975b0937edc7cf8dd1d
Benjamin Johnston <benjohnstonsf@gmail.com>
2015-11-04 23:20:37 -0800

    Initial Commit

---
+++ OnTheMap/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  OnTheMap
+//
+//  Created by Benjamin Johnston on 11/4/15.
+//  Copyright © 2015 Benjamin Johnston. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
